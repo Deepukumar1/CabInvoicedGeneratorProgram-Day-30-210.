760f1b4d1e9b374cff5c34684f0e19721ac0736b
Deepukumar1 <deepukumarmahasheth2@gmail.com>
2022-12-04 17:14:50 +0530

    Added: return minumum fair

--- src/com/bridgelabz/main/RideRepository.java
+++ src/com/bridgelabz/main/RideRepository.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RideRepository {
-	 public static List<Customer> customerList = new ArrayList<>();
+	 static List<Customer> customerList = new ArrayList<>();
 
 	    public static Invoice generateInvoiceByUserId(int userId) {
 	        for (Customer customer : customerList) {
